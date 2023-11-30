1040e80ecb2ff1cae68bac2db883cf1f244881aa
AMPmiq <miquelosouza@gmail.com>
2023-11-29 21:23:08 -0300

    fix

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ from app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
